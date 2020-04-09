45d47d83bc7fa4a5a13ca77b0313b637ebd622a0
Madison <schottmadison@Madisons-MacBook-Air.local>
2020-04-08 22:08:43 -0400

    find variables using wae and iv

---
+++ variable_selection.R
@@ -0,0 +1,17 @@
+install.packages("Information", repos = "http://cran.us.r-project.org")
+install.packages("tidyr", repos = "http://cran.us.r-project.org")
+
+electronics <- read.csv(file = 'electronics.csv')
+profiles <- read.csv(file = 'profile.csv')
+customer_info <- merge(electronics, profiles, by='clients')
+
+for (column in colnames(customer_info)){
+  if (!(column == "got_a_TV" )){
+    customer_info[[column]] <- factor(customer_info[[column]])
+  }
+}
+
+IV <- Information::create_infotables(data=customer_info, y="got_a_TV", bins=10, parallel=FALSE)
+IV_value = data.frame(IV$Summary)
+print(IV_value)
+Information::plot_infotables(IV, IV$Summary$Variable[1:23], same_scale=FALSE, show_values=TRUE)
